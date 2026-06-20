a6884b7fe6b2f1437d6d0ec11261bae55264ef5d
Anshyadavvvv <anshyadav9709@gmail.com>
2026-06-21 02:39:03 +0530

    Refactor main function and improve logic and resolve merge conflict

--- Problem12.cpp
+++ Problem12.cpp
@@ -1,82 +1,34 @@
-<<<<<<< HEAD
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    
-
-    int t;
+int main()
+{
+    int t; 
     cin >> t;
-
-    while (t--) {
-        int n;
+    while (t--)
+    {
+        int n; 
         cin >> n;
-
-        vector<int> b(n);
-        for (int i = 0; i < n; i++) {
+        vector<int> b(n), a; 
+        for (int i = 0; i < n; i++) 
             cin >> b[i];
-        }
-
-        vector<int> a;
-        a.push_back(b[0]);
-
-        for (int i = 0; i < n - 1; i++) {
-            if (b[i] <= b[i + 1]) {
-                a.push_back(b[i + 1]);
-            } else {
-                a.push_back(1);
-                a.push_back(b[i + 1]);
+        
+        a.push_back(b[0]); 
+        for (int i = 1; i < n; i++) 
+        {
+            if (b[i] >= b[i - 1])
+                a.push_back(b[i]); 
+            else
+            {
+                a.push_back(b[i]); 
+                a.push_back(b[i]); 
             }
         }
-
-        cout << a.size() << '\n';
-
-        for (int x : a) {
-            cout << x << " ";
-        }
-        cout << '\n';
+        cout << a.size() << endl; 
+        for (auto it : a) 
+            cout << it << " ";
+        cout << endl; 
     }
-
     return 0;
-=======
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    
-
-    int t;
-    cin >> t;
-
-    while (t--) {
-        int n;
-        cin >> n;
-
-        vector<int> b(n);
-        for (int i = 0; i < n; i++) {
-            cin >> b[i];
-        }
-
-        vector<int> a;
-        a.push_back(b[0]);
-
-        for (int i = 0; i < n - 1; i++) {
-            if (b[i] <= b[i + 1]) {
-                a.push_back(b[i + 1]);
-            } else {
-                a.push_back(1);
-                a.push_back(b[i + 1]);
-            }
-        }
-
-        cout << a.size() << '\n';
-
-        for (int x : a) {
-            cout << x << " ";
-        }
-        cout << '\n';
-    }
-
-    return 0;
->>>>>>> f137f7601dab08ce9be3599d1cfa67b74b6a65a9
 }
+
