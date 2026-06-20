423eb3730a3b791e62fda4e6e9ab887810ade673
Anshyadavvvv <anshyadav9709@gmail.com>
2026-06-20 17:33:17 +0530

    Fix merge conflict in Problem7.cpp

--- Problem7.cpp
+++ Problem7.cpp
@@ -1,38 +1,4 @@
-<<<<<<< HEAD
-#include <bits/stdc++.h>
-using namespace std;
 
-int main() {
-   
-
-    int t;
-    cin >> t;
-
-    while (t--) {
-        int n, m;
-        cin >> n >> m;
-
-        string x, s;
-        cin >> x >> s;
-
-        int ans = -1;
-
-        for (int i = 0; i <= 6; i++) {
-
-            if (x.find(s) != string::npos) {
-                ans = i;
-                break;
-            }
-
-            x += x;   // perform one operation
-        }
-
-        cout << ans << '\n';
-    }
-
-    return 0;
-}
-=======
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -64,4 +30,4 @@ int main() {
 
     return 0;
 }
->>>>>>> f137f7601dab08ce9be3599d1cfa67b74b6a65a9
+
