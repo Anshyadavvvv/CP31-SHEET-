744c66f544932aef202f7d7d6d292afea6652e0b
Anshyadavvvv <anshyadav9709@gmail.com>
2026-06-21 03:05:51 +0530

    Update Problem28.cpp
    Refactor main function and improve input handling.

--- Problem28.cpp
+++ Problem28.cpp
@@ -1,47 +1,35 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int t;
+int main()
+{
+    int t; 
     cin >> t;
-
-    while (t--) {
+    while (t--)
+    {
         int n;
         cin >> n;
+        int a[n]; 
+        for (int i = 0; i < n; i++) 
+            { cin >> a[i]; }
+       
 
-        int a[55];
-
-        for (int i = 0; i < n; i++) {
-            cin >> a[i];
+        sort(a, a + n); 
+        int maxi = a[n - 1]; 
+         int mini = a[0]; 
+        if (maxi == mini) 
+        {
+            cout << "NO" << endl; 
         }
-
-        bool same = true;
-
-        for (int i = 1; i < n; i++) {
-            if (a[i] != a[0]) {
-                same = false;
-                break;
-            }
+        else
+        {
+            cout << "YES" << endl; 
+            cout << maxi << " "; 
+            for (int i = 0; i < n - 1; i++) 
+                cout << a[i] << " ";
+            cout << endl; 
         }
-
-        if (same) {
-            cout << "NO\n";
-            continue;
-        }
-
-        sort(a, a + n);
-
-        cout << "YES\n";
-
-        cout << a[n - 1] << " ";
-
-        for (int i = 0; i < n - 1; i++) {
-            cout << a[i] << " ";
-        }
-
-        cout << "\n";
     }
-
     return 0;
 }
+
