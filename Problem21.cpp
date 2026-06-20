134aea53e36a9dc220d06e5bc9b2086836e1274c
Anshyadavvvv <anshyadav9709@gmail.com>
2026-06-21 02:41:52 +0530

    resolve merge conflicts

--- Problem21.cpp
+++ Problem21.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -23,30 +22,3 @@ int main() {
         cout<<ans<<endl;
     }
     return 0;
-=======
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        vector<int> A(n);
-        for(int i = 0 ; i < n ; i++){
-            cin>>A[i];
-        }
-        int cnt = 0;
-        int ans = 0;
-        for(int i = 0 ; i < n ; i++){
-            if(A[i] == 0){
-                cnt++;
-                ans = max(ans,cnt);
-            }else{cnt = 0;}
-        }
-        cout<<ans<<endl;
-    }
-    return 0;
->>>>>>> f137f7601dab08ce9be3599d1cfa67b74b6a65a9
-}
